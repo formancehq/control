557e9a6d3912cb61fcd4a9439ca30539177302e5
reslene <reslene@numary.com>
2022-08-17 02:51:06 +0200

    refactor(): clean useless file

--- app/src/hooks/useSearchUrl.ts
+++
@@ -1,7 +0,0 @@
-import { useLocation } from 'react-router-dom';
-
-export function useSearchUrl(): { [k: string]: string } {
-  const location = useLocation();
-
-  return Object.fromEntries(new URLSearchParams(location.search));
-}
