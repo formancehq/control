2c414eb9c69f231200bfed64bf4764f7dae9ecfb
Sofiane <sofiane@formance.com>
2022-08-24 11:22:28 +0200

    feat(JsonViewer): Remove table and add a custom container + changed the type of metadata

--- app/src/components/Wrappers/Metadata/types.ts
+++ app/src/components/Wrappers/Metadata/types.ts
@@ -1,7 +1,7 @@
 import { LedgerResources, Metadata } from '~/src/types/ledger';
 
 export type MetadataProps = {
-  metadata: Metadata[];
+  metadata: Metadata;
   title: string;
   resource: LedgerResources.ACCOUNTS | LedgerResources.TRANSACTIONS;
   id: string;
