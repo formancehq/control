06ec26aa95a1f120defff9c7b40c665fa142f94b
reslene <reslene@numary.com>
2022-08-25 17:38:54 -0400

    feat(Search): adjust key event + improve UI with grid
    Signed-off-by: reslene <reslene@numary.com>

--- app/src/components/Wrappers/Lists/AccountList/AccountList.tsx
+++ app/src/components/Wrappers/Lists/AccountList/AccountList.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { Account } from '~/src/types/ledger';
 import { useNavigate } from 'react-router-dom';
 import { getLedgerAccountDetailsRoute } from '~/src/components/Navbar/routes';
-import { Row, SourceDestination } from '@numaryhq/storybook';
+import { Chip, Row, SourceDestination } from '@numaryhq/storybook';
 import { AccountListProps } from '~/src/components/Wrappers/Lists/AccountList/types';
 import { useTranslation } from 'react-i18next';
 import Table from '~/src/components/Wrappers/Table';
@@ -10,15 +10,6 @@ import Table from '~/src/components/Wrappers/Table';
 const AccountList: FunctionComponent<AccountListProps> = ({ accounts }) => {
   const navigate = useNavigate();
   const { t } = useTranslation();
-
-  const renderAddress = (account: Account) => (
-    <SourceDestination
-      ellipse={false}
-      key={account.address}
-      label={account.address}
-      onClick={() => handleAction(account)}
-    />
-  );
 
   const handleAction = (account: Account) =>
     navigate(getLedgerAccountDetailsRoute(account.address, account.ledger));
@@ -31,9 +22,30 @@ const AccountList: FunctionComponent<AccountListProps> = ({ accounts }) => {
           key: 'address',
           label: t('pages.accounts.table.columnLabel.address'),
         },
+        {
+          key: 'ledger',
+          label: t('pages.accounts.table.columnLabel.ledger'),
+        },
       ]}
       renderItem={(account: Account, index) => (
-        <Row key={index} keys={[renderAddress]} item={account} />
+        <Row
+          key={index}
+          keys={[
+            <SourceDestination
+              ellipse={false}
+              key={account.address}
+              label={account.address}
+              onClick={() => handleAction(account)}
+            />,
+            <Chip
+              key={index}
+              label={account.ledger}
+              variant="square"
+              color="brown"
+            />,
+          ]}
+          item={account}
+        />
       )}
     />
   );
