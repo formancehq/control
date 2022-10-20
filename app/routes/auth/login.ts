a00c37f86c701dc5c327c256da6db7761ff1e8d3
reslene <reslene@numary.com>
2022-10-20 17:32:01 +0200

    fix(Auth): use redirect uri from .env instead of url.origin object.
    Signed-off-by: reslene <reslene@numary.com>

--- app/routes/auth/login.ts
+++ app/routes/auth/login.ts
@@ -1,5 +1,5 @@
-import { json, redirect } from '@remix-run/node';
-import { LoaderFunction, TypedResponse } from '@remix-run/server-runtime';
+import { json, redirect } from "@remix-run/node";
+import { LoaderFunction, TypedResponse } from "@remix-run/server-runtime";
 
 import {
   commitSession,
@@ -9,27 +9,27 @@ import {
   getOpenIdConfig,
   getSession,
   State,
-} from '~/src/utils/auth.server';
+} from "~/src/utils/auth.server";
 
 export const loader: LoaderFunction = async ({
   request,
 }): Promise<TypedResponse> => {
-  const session = await getSession(request.headers.get('Cookie'));
+  const session = await getSession(request.headers.get("Cookie"));
   const url = new URL(request.url);
-  const code = url.searchParams.get('code');
-  const stateBase64 = url.searchParams.get('state');
-  const buff = new Buffer(stateBase64!, 'base64');
-  const state: State = JSON.parse(buff.toString('ascii'));
+  const code = url.searchParams.get("code");
+  const stateBase64 = url.searchParams.get("state");
+  const buff = new Buffer(stateBase64!, "base64");
+  const state: State = JSON.parse(buff.toString("ascii"));
   const openIdConfig = await getOpenIdConfig();
   if (code) {
     // get through authentication callback
-    const authentication = await exchangeToken(openIdConfig, code, url);
+    const authentication = await exchangeToken(openIdConfig, code);
     const encryptedCookie = encrypt(authentication);
     session.set(COOKIE_NAME, encryptedCookie);
 
-    return redirect(state.redirectTo || '/', {
+    return redirect(state.redirectTo || "/", {
       headers: {
-        'Set-Cookie': await commitSession(session),
+        "Set-Cookie": await commitSession(session),
       },
     });
   }
