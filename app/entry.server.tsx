b411d8658c879b3a980fcf10b197cb4ea0152917
reslene <reslene@numary.com>
2022-10-20 13:10:44 +0200

    feat(OpenTel): add dep + default setup
    Signed-off-by: reslene <reslene@numary.com>

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,8 +3,13 @@ import * as React from 'react';
 import { CacheProvider } from '@emotion/react';
 import createEmotionServer from '@emotion/server/create-instance';
 import { ThemeProvider } from '@mui/material/styles';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 import { RemixServer } from '@remix-run/react';
 import type { EntryContext } from '@remix-run/server-runtime';
+import { RemixInstrumentation } from 'opentelemetry-instrumentation-remix';
 import { renderToString } from 'react-dom/server';
 
 import createEmotionCache from './src/utils/createEmotionCache';
@@ -19,6 +24,33 @@ export default async function handleRequest(
 ) {
   const cache = createEmotionCache();
   const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  const provider = new NodeTracerProvider();
+  provider.register();
+
+  registerInstrumentations({
+    instrumentations: [new RemixInstrumentation()],
+  });
+
+  const collectorOptions = {
+    url: '<opentelemetry-collector-url>', // url is optional and can be omitted - default is http://localhost:4318/v1/traces
+    headers: {}, // an optional object containing custom headers to be sent with each request
+    concurrencyLimit: 10, // an optional limit on pending requests
+  };
+
+  const exporter = new OTLPTraceExporter(collectorOptions);
+  provider.addSpanProcessor(
+    new BatchSpanProcessor(exporter, {
+      // The maximum queue size. After the size is reached spans are dropped.
+      maxQueueSize: 100,
+      // The maximum batch size of every export. It must be smaller or equal to maxQueueSize.
+      maxExportBatchSize: 10,
+      // The interval between two consecutive exports
+      scheduledDelayMillis: 500,
+      // How long the export can run before it is cancelled
+      exportTimeoutMillis: 30000,
+    })
+  );
 
   const MuiRemixServer = () => (
     <CacheProvider value={cache}>
