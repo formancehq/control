95e6e32a3e9873b47e462b65c89390e04ed2e5cd
reslene <reslene@numary.com>
2022-10-21 11:27:10 +0200

    feat(opentel): adjust config
    Signed-off-by: reslene <reslene@numary.com>

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,10 +3,13 @@ import * as React from 'react';
 import { CacheProvider } from '@emotion/react';
 import createEmotionServer from '@emotion/server/create-instance';
 import { ThemeProvider } from '@mui/material/styles';
+import { trace } from '@opentelemetry/api';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { Resource } from '@opentelemetry/resources';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { RemixServer } from '@remix-run/react';
 import type { EntryContext } from '@remix-run/server-runtime';
 import { RemixInstrumentation } from 'opentelemetry-instrumentation-remix';
@@ -25,32 +28,31 @@ export default async function handleRequest(
   const cache = createEmotionCache();
   const { extractCriticalToChunks } = createEmotionServer(cache);
 
-  const provider = new NodeTracerProvider();
-  provider.register();
-
-  registerInstrumentations({
-    instrumentations: [new RemixInstrumentation()],
-  });
-
+  // otel
   const collectorOptions = {
-    url: '<opentelemetry-collector-url>', // url is optional and can be omitted - default is http://localhost:4318/v1/traces
-    headers: {}, // an optional object containing custom headers to be sent with each request
+    url: process.env.OPENTEL_COLLECTOR,
     concurrencyLimit: 10, // an optional limit on pending requests
   };
-
   const exporter = new OTLPTraceExporter(collectorOptions);
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: 'control',
+    }),
+  });
   provider.addSpanProcessor(
     new BatchSpanProcessor(exporter, {
-      // The maximum queue size. After the size is reached spans are dropped.
       maxQueueSize: 100,
-      // The maximum batch size of every export. It must be smaller or equal to maxQueueSize.
       maxExportBatchSize: 10,
-      // The interval between two consecutive exports
       scheduledDelayMillis: 500,
-      // How long the export can run before it is cancelled
       exportTimeoutMillis: 30000,
     })
   );
+  provider.register();
+  registerInstrumentations({
+    instrumentations: [new RemixInstrumentation()],
+    tracerProvider: provider,
+  });
+  trace.getTracer('control');
 
   const MuiRemixServer = () => (
     <CacheProvider value={cache}>
