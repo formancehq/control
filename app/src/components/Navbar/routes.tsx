d41ec48d821bf09881e7aaec11ca8188c665594b
Sofiane <sofiane@formance.com>
2022-10-24 19:10:58 +0200

    feat(SubRouteConnectors): Add Subroute for connectors route

--- app/src/components/Navbar/routes.tsx
+++ app/src/components/Navbar/routes.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
-import { AccountBalance, CreditCard, Home, Share } from "@mui/icons-material";
+import { AccountBalance, CreditCard, Home, Share } from '@mui/icons-material';
 
-export const ROOT_ROUTE = "/";
-export const OVERVIEW_ROUTE = "/overview";
-export const ACCOUNT_ROUTE = "/ledgers/:slug/accounts/:id";
-export const TRANSACTION_ROUTE = "/ledgers/:slug/transactions/:id";
-export const PAYMENTS_ROUTE = "/payments";
-export const PAYMENT_ROUTE = "/payments/:id";
-export const ACCOUNTS_ROUTE = "/accounts";
-export const TRANSACTIONS_ROUTE = "/transactions";
-export const CONNECTORS_ROUTE = "/connectors";
+export const ROOT_ROUTE = '/';
+export const OVERVIEW_ROUTE = '/overview';
+export const ACCOUNT_ROUTE = '/ledgers/:slug/accounts/:id';
+export const TRANSACTION_ROUTE = '/ledgers/:slug/transactions/:id';
+export const PAYMENTS_ROUTE = '/payments';
+export const PAYMENT_ROUTE = '/payments/:id';
+export const ACCOUNTS_ROUTE = '/accounts';
+export const TRANSACTIONS_ROUTE = '/transactions';
+export const CONNECTORS_ROUTE = '/connectors';
 
 export const getRoute = (uri: string, id?: number | string): string =>
   id !== undefined ? uri.replace(/:\w+/, id.toString(10)) : uri;
@@ -39,36 +39,36 @@ export type RouterConfig = {
 };
 
 export const overview: RouterConfig = {
-  id: "overview",
-  label: "navbar.title.overview",
+  id: 'overview',
+  label: 'navbar.title.overview',
   path: [getRoute(OVERVIEW_ROUTE), ROOT_ROUTE],
   icon: <Home />,
 };
 
 export const payments: RouterConfig = {
-  id: "payments",
-  label: "navbar.title.payments",
+  id: 'payments',
+  label: 'navbar.title.payments',
   path: getRoute(PAYMENTS_ROUTE),
   icon: <CreditCard />,
 };
 
 export const accounts: RouterConfig = {
-  id: "accounts",
-  label: "navbar.title.accounts",
+  id: 'accounts',
+  label: 'navbar.title.accounts',
   path: getRoute(ACCOUNTS_ROUTE),
   icon: <AccountBalance />,
 };
 
 export const transactions: RouterConfig = {
-  id: "transactions",
-  label: "navbar.title.transactions",
+  id: 'transactions',
+  label: 'navbar.title.transactions',
   path: getRoute(TRANSACTIONS_ROUTE),
   icon: <AccountBalance />,
 };
 
 export const connectors: RouterConfig = {
-  id: "connectors",
-  label: "navbar.title.connectors",
+  id: 'connectors',
+  label: 'navbar.title.connectors',
   path: getRoute(CONNECTORS_ROUTE),
   icon: <Share />,
 };
