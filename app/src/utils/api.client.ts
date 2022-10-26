56b7e39a865a47497d76ea1dc4ab01f5ae60b810
reslene <reslene@numary.com>
2022-10-26 13:11:35 +0200

    chore(): improve logging
    Signed-off-by: reslene <reslene@numary.com>

--- app/src/utils/api.client.ts
+++ app/src/utils/api.client.ts
@@ -1,24 +1,26 @@
-import { ApiClient, Methods } from '~/src/utils/api';
+import { ApiClient, logger, Methods, toJson } from '~/src/utils/api';
 
 export class ReactApiClient implements ApiClient {
   getResource<T>(
     params: string,
     path?: string | undefined
-  ): Promise<T | undefined> {
+  ): Promise<T | undefined | void> {
     return fetch('/proxify', {
       method: Methods.POST,
       body: JSON.stringify({
         params,
         path,
       }),
-    }).then((response) => response.json());
+    })
+      .then(async (response) => await toJson<T>(response))
+      .catch((e: any) => logger(e, 'api.client', { params, path }));
   }
 
   postResource<T>(
     params: string,
     body: any,
     path: string | undefined
-  ): Promise<T | undefined> {
+  ): Promise<T | undefined | void> {
     return fetch('/proxify', {
       method: Methods.POST,
       body: JSON.stringify({
@@ -26,19 +28,23 @@ export class ReactApiClient implements ApiClient {
         path,
         body,
       }),
-    }).then((response) => response.json());
+    })
+      .then(async (response) => await toJson<T>(response))
+      .catch((e: any) => logger(e, 'api.client', { params, body }));
   }
 
   deleteResource<T>(
     params: string,
     path: string | undefined
-  ): Promise<T | undefined> {
+  ): Promise<T | undefined | void> {
     return fetch('/proxify', {
       method: Methods.DELETE,
       body: JSON.stringify({
         params,
         path,
       }),
-    }).then((response) => response.json());
+    })
+      .then(async (response) => await toJson<T>(response))
+      .catch((e: any) => logger(e, 'api.client', { params }));
   }
 }
