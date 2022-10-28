e7ebc56fc8f94ddb0f1f3109d8756b8c0e578bd8
Sofiane <sofiane@formance.com>
2022-10-28 12:32:53 +0200

    feat(AppsPage): Add a string format on form input name

--- app/routes/connectors/apps/CreateConnectorsForm.tsx
+++ app/routes/connectors/apps/CreateConnectorsForm.tsx
@@ -1,12 +1,13 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
 
 import { Add } from '@mui/icons-material';
 import { isEmpty } from 'lodash';
 import { Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
-import { LoadingButton, Modal, Select } from '@numaryhq/storybook';
+import { Select } from '@numaryhq/storybook';
 
+import Modal from '~/src/components/Wrappers/Modal';
 import { buildForm } from '~/src/utils/formBuilder/formBuilder';
 
 const connectorsConfig = {
@@ -57,7 +58,6 @@ const connectorsConfig = {
 
 export const CreateConnectorsForm: FunctionComponent = () => {
   const { t } = useTranslation();
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const formattedConnectorConfig = Object.keys(connectorsConfig).map((key) => ({
     id: key,
@@ -73,18 +73,23 @@ export const CreateConnectorsForm: FunctionComponent = () => {
     reset,
     watch,
     handleSubmit,
-  } = useForm<any>({ mode: 'onChange' });
+  } = useForm<any>({
+    mode: 'onChange',
+    defaultValues: {
+      connectorSelect: '',
+      modulr: {
+        apiKey: '',
+        apiSecret: '',
+        endpoint: '',
+      },
+    },
+  });
 
   const connectorKey = watch('connectorSelect');
 
-  const handleClosingModal = () => {
-    setIsModalOpen(false);
-    reset();
-  };
-
-  const handleOpeningModal = () => {
-    setIsModalOpen(true);
-  };
+  // const handleOpeningModal = () => {
+  //   setIsModalOpen(true);
+  // };
 
   const onSave = async () => {
     await trigger();
@@ -95,64 +100,60 @@ export const CreateConnectorsForm: FunctionComponent = () => {
     }
     console.log('closed');
     reset();
-    handleClosingModal();
   };
 
   useEffect(() => {
-    console.log('connectorKey', connectorKey);
     clearErrors();
   }, [connectorKey]);
 
   return (
-    <>
-      <LoadingButton
-        id="create"
-        variant="dark"
-        endIcon={<Add />}
-        content={t('pages.connectors.tabs.apps.pageButton.actionLabel')}
-        onClick={handleOpeningModal}
-      />
-
-      <Modal
-        open={isModalOpen}
-        onClose={() => handleClosingModal()}
-        actions={{
+    <Modal
+      button={{
+        id: 'create',
+        variant: 'dark',
+        startIcon: <Add />,
+        content: t('pages.connectors.tabs.apps.pageButton.actionLabel'),
+      }}
+      modal={{
+        id: 'create-apps-modal',
+        PaperProps: { sx: { minWidth: '500px' } },
+        title: t('common.dialog.createTitle'),
+        actions: {
           cancel: {
-            onClick: handleClosingModal,
-            label: t('common.dialog.cancelButton'),
+            onClick: async () => {
+              reset();
+            },
           },
           save: {
             onClick: onSave,
-            label: t('common.dialog.saveButton'),
             disabled: !isValid,
           },
-        }}
-        title={t('common.dialog.createTitle')}
-        PaperProps={{ sx: { width: '500px' } }}
-      >
-        <form onSubmit={handleSubmit(onSave)}>
-          <Controller
-            name="connectorSelect"
-            control={control}
-            rules={{ required: true }}
-            render={({ field: { ref, onChange, ...rest } }) => (
-              <Select
-                items={formattedConnectorConfig}
-                {...rest}
-                // TODO replace trad
-                placeholder="Select a connector"
-                error={!!errors.connectorSelect}
-                select={{
-                  ref: ref,
-                  inputRef: ref,
-                  onChange: onChange,
-                }}
-              />
-            )}
-          />
-          {connectorKey && buildForm({ connectorKey, control, errors })}
-        </form>
-      </Modal>
-    </>
+        },
+      }}
+    >
+      <form onSubmit={handleSubmit(onSave)}>
+        <Controller
+          name="connectorSelect"
+          control={control}
+          rules={{ required: true }}
+          render={({ field: { ref, onChange, ...rest } }) => (
+            <Select
+              {...rest}
+              items={formattedConnectorConfig}
+              placeholder={t('common.forms.selectEntity', {
+                entityName: 'connector',
+              })}
+              error={!!errors.connectorSelect}
+              select={{
+                ref: ref,
+                inputRef: ref,
+                onChange: onChange,
+              }}
+            />
+          )}
+        />
+        {connectorKey && buildForm({ connectorKey, control, errors })}
+      </form>
+    </Modal>
   );
 };
