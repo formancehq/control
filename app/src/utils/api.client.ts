c57049b57247ae291ba1a7cc45e34765830867df
reslene <reslene@numary.com>
2022-11-02 17:23:32 +0100

    feat(WebhookList): handle activation action
    Signed-off-by: reslene <reslene@numary.com>

--- app/src/utils/api.client.ts
+++ app/src/utils/api.client.ts
@@ -1,6 +1,6 @@
-import { get } from 'lodash';
+import { get } from "lodash";
 
-import { ApiClient, logger, Methods, toJson } from '~/src/utils/api';
+import { ApiClient, logger, Methods, toJson } from "~/src/utils/api";
 
 export class ReactApiClient implements ApiClient {
   getResource<T>(
@@ -8,6 +8,13 @@ export class ReactApiClient implements ApiClient {
     path?: string | undefined
   ): Promise<T | undefined | void> {
     return this.handleRequest<T>(Methods.GET, params, undefined, path);
+  }
+
+  putResource<T>(
+    params: string,
+    path?: string | undefined
+  ): Promise<T | undefined | void> {
+    return this.handleRequest<T>(Methods.PUT, params, undefined, path);
   }
 
   postResource<T>(
@@ -31,7 +38,7 @@ export class ReactApiClient implements ApiClient {
     body?: any,
     path?: string
   ): Promise<T | undefined | void> {
-    return fetch('/proxify', {
+    return fetch("/proxify", {
       method: Methods.POST,
       body: JSON.stringify({
         params,
@@ -46,8 +53,8 @@ export class ReactApiClient implements ApiClient {
         return path ? get(json, path, json) : json;
       })
       .catch((e: any) => {
-        logger(e, 'api.client', { params, path, body, method });
-        throw new Error('Error');
+        logger(e, "api.client", { params, path, body, method });
+        throw new Error("Error");
       });
   }
 }
