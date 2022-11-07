e8a48784c941a79c6a2fc148b5b3aec41196a81e
reslene <reslene@numary.com>
2022-11-07 16:28:20 +0100

    fix(Auth): use window location instead of navigate for logout
    Signed-off-by: reslene <reslene@numary.com>

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import { ReactElement, useEffect, useState } from 'react';
+import * as React from "react";
+import { ReactElement, useEffect, useState } from "react";
 
-import { withEmotionCache } from '@emotion/react';
-import { Home, Logout } from '@mui/icons-material';
+import { withEmotionCache } from "@emotion/react";
+import { Home, Logout } from "@mui/icons-material";
 import {
   Backdrop,
   Box,
   CircularProgress,
   Typography,
   unstable_useEnhancedEffect as useEnhancedEffect,
-} from '@mui/material';
-import { redirect } from '@remix-run/node';
+} from "@mui/material";
+import { redirect } from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -20,29 +20,29 @@ import {
   ScrollRestoration,
   useCatch,
   useLoaderData,
-} from '@remix-run/react';
-import { LinksFunction, LoaderFunction } from '@remix-run/server-runtime';
-import { camelCase, get } from 'lodash';
-import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
+} from "@remix-run/react";
+import { LinksFunction, LoaderFunction } from "@remix-run/server-runtime";
+import { camelCase, get } from "lodash";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 
-import styles from './root.css';
-import { useOpen } from './src/hooks/useOpen';
+import styles from "./root.css";
+import { useOpen } from "./src/hooks/useOpen";
 
-import { LoadingButton, Snackbar, theme } from '@numaryhq/storybook';
+import { LoadingButton, Snackbar, theme } from "@numaryhq/storybook";
 
-import Layout from '~/src/components/Layout';
-import { getRoute, OVERVIEW_ROUTE } from '~/src/components/Navbar/routes';
-import ClientStyleContext from '~/src/contexts/clientStyleContext';
-import { ServiceContext } from '~/src/contexts/service';
+import Layout from "~/src/components/Layout";
+import { getRoute, OVERVIEW_ROUTE } from "~/src/components/Navbar/routes";
+import ClientStyleContext from "~/src/contexts/clientStyleContext";
+import { ServiceContext } from "~/src/contexts/service";
 import {
   Authentication,
   CurrentUser,
   errorsMap,
   logger,
-} from '~/src/utils/api';
-import { ReactApiClient } from '~/src/utils/api.client';
-import { createApiClient } from '~/src/utils/api.server';
+} from "~/src/utils/api";
+import { ReactApiClient } from "~/src/utils/api.client";
+import { createApiClient } from "~/src/utils/api.server";
 import {
   AUTH_CALLBACK_ROUTE,
   COOKIE_NAME,
@@ -56,22 +56,23 @@ import {
   refreshToken,
   State,
   withSession,
-} from '~/src/utils/auth.server';
+} from "~/src/utils/auth.server";
+import { useService } from "~/src/hooks/useService";
 
 interface DocumentProps {
   children: React.ReactNode;
   title?: string;
 }
 
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const session = await getSession(request.headers.get('Cookie'));
+  const session = await getSession(request.headers.get("Cookie"));
   const cookie = session.get(COOKIE_NAME);
   const url = new URL(request.url);
   const stateObject: State = { redirectTo: `${url.pathname}${url.search}` };
   const buff = new Buffer(JSON.stringify(stateObject));
-  const stateAsBase64 = buff.toString('base64');
+  const stateAsBase64 = buff.toString("base64");
   const openIdConfig = await getOpenIdConfig();
 
   if (!cookie) {
@@ -92,18 +93,18 @@ export const loader: LoaderFunction = async ({ request }) => {
       );
       if (refresh.access_token) {
         session.set(COOKIE_NAME, encrypt(refresh));
-        const api = await createApiClient(session, '');
+        const api = await createApiClient(session, "");
         currentUser = await api.getResource<CurrentUser>(
           openIdConfig.userinfo_endpoint
         );
         const payload = getJwtPayload(sessionHolder);
         const pseudo =
           currentUser && currentUser.email
-            ? currentUser.email.split('@')[0]
+            ? currentUser.email.split("@")[0]
             : undefined;
         currentUser = {
           ...currentUser,
-          avatarLetter: pseudo ? pseudo.split('')[0].toUpperCase() : undefined,
+          avatarLetter: pseudo ? pseudo.split("")[0].toUpperCase() : undefined,
           pseudo,
           scp: payload ? payload.scp : [],
         };
@@ -119,6 +120,71 @@ export const loader: LoaderFunction = async ({ request }) => {
     })
   );
 };
+
+const renderError = (
+  navigate: NavigateFunction,
+  t: any,
+  origin: string,
+  message?: string,
+  description?: string
+): ReactElement => (
+  <Backdrop
+    sx={{
+      zIndex: (theme) => theme.zIndex.drawer + 1,
+    }}
+    open={true}
+  >
+    <Box
+      display="flex"
+      justifyContent="space-evenly"
+      sx={{
+        width: "100%",
+        height: "100%",
+        background: ({ palette }) => palette.neutral[0],
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          alignSelf: "center",
+          background: ({ palette }) => palette.neutral[0],
+        }}
+      >
+        <Typography variant="large3x" mb={3}>
+          {t("common.boundaries.title")}
+        </Typography>
+        <Typography variant="h2" mt={3}>
+          {message || t("common.boundaries.errorState.error.title")}
+        </Typography>
+        <Typography variant="body2" mt={3}>
+          {description || t("common.boundaries.errorState.error.description")}
+        </Typography>
+        <Box sx={{ display: "flex" }}>
+          <LoadingButton
+            id="go-back-home"
+            content="Go back home"
+            variant="primary"
+            startIcon={<Home />}
+            onClick={() => navigate(getRoute(OVERVIEW_ROUTE))}
+            sx={{ mt: 5, mr: 1 }}
+          />
+          <LoadingButton
+            id="logout"
+            content={t("topbar.logout")}
+            variant="stroke"
+            startIcon={<Logout />}
+            onClick={() => {
+              window.location.href = `${origin}/auth/redirect-logout`;
+            }}
+            sx={{ mt: 5 }}
+          />
+        </Box>
+      </Box>
+    </Box>
+  </Backdrop>
+);
 
 const Document = withEmotionCache(
   ({ children, title }: DocumentProps, emotionCache) => {
@@ -144,7 +210,7 @@ const Document = withEmotionCache(
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width,initial-scale=1" />
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <title>{title || 'Formance'}</title>
+          <title>{title || "Formance"}</title>
           <Meta />
           <link
             rel="stylesheet"
@@ -174,70 +240,6 @@ const Document = withEmotionCache(
   }
 );
 
-const renderError = (
-  navigate: NavigateFunction,
-  t: any,
-  message?: string,
-  description?: string
-): ReactElement => (
-  <Backdrop
-    sx={{
-      zIndex: (theme) => theme.zIndex.drawer + 1,
-    }}
-    open={true}
-  >
-    <Box
-      display="flex"
-      justifyContent="space-evenly"
-      sx={{
-        width: '100%',
-        height: '100%',
-        background: ({ palette }) => palette.neutral[0],
-      }}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'flex-start',
-          alignSelf: 'center',
-          background: ({ palette }) => palette.neutral[0],
-        }}
-      >
-        <Typography variant="large3x" mb={3}>
-          {t('common.boundaries.title')}
-        </Typography>
-        <Typography variant="h2" mt={3}>
-          {message || t('common.boundaries.errorState.error.title')}
-        </Typography>
-        <Typography variant="body2" mt={3}>
-          {description || t('common.boundaries.errorState.error.description')}
-        </Typography>
-        <Box sx={{ display: 'flex' }}>
-          <LoadingButton
-            id="go-back-home"
-            content="Go back home"
-            variant="primary"
-            startIcon={<Home />}
-            onClick={() => navigate(getRoute(OVERVIEW_ROUTE))}
-            sx={{ mt: 5, mr: 1 }}
-          />
-          <LoadingButton
-            id="logout"
-            content={t('topbar.logout')}
-            variant="stroke"
-            startIcon={<Logout />}
-            onClick={() => {
-              navigate('auth/redirect-logout');
-            }}
-            sx={{ mt: 5 }}
-          />
-        </Box>
-      </Box>
-    </Box>
-  </Backdrop>
-);
-
 // https://remix.run/api/conventions#default-export
 // https://remix.run/api/conventions#route-filenames
 export default function App() {
@@ -247,10 +249,10 @@ export default function App() {
   const [loading, _load, stopLoading] = useOpen(true);
   const [feedback, setFeedback] = useState({
     active: false,
-    message: t('common.feedback.error'),
+    message: t("common.feedback.error"),
   });
 
-  const displayFeedback = (message = 'common.feedback.error') => {
+  const displayFeedback = (message = "common.feedback.error") => {
     setFeedback({ active: true, message: t(message) });
   };
 
@@ -258,7 +260,7 @@ export default function App() {
     event: React.SyntheticEvent | Event,
     reason?: string
   ) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
@@ -270,9 +272,9 @@ export default function App() {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     if (!global.timer) {
-      console.info('Global time not defined, installing it');
+      console.info("Global time not defined, installing it");
       const refreshToken = (): Promise<any> => {
-        console.info('Trigger refresh authentication');
+        console.info("Trigger refresh authentication");
 
         return fetch(`${metas.origin}/auth/refresh`)
           .then((response) => response.json())
@@ -281,22 +283,22 @@ export default function App() {
           )
           .catch(async (reason) => {
             if (reason?.status === 400) {
-              console.info('End session');
-              navigate('auth/redirect-logout');
+              console.info("End session");
+              window.location.href = `${origin}/auth/redirect-logout`;
             } else {
               // retry one last time
-              console.info('Retry refresh');
+              console.info("Retry refresh");
               const refresh = await fetch(`${metas.origin}/auth/refresh`);
               switch (refresh?.status) {
                 case 200:
                   return refresh.json();
                 case 500:
-                  return await fetch(`${metas.origin}/auth/logout`);
+                  return (window.location.href = `${origin}/auth/logout`);
                 default:
-                  return navigate('/auth/redirect-logout');
+                  return (window.location.href = `${origin}/auth/redirect-logout`);
               }
             }
-            console.info('Error refreshing token: ', reason);
+            console.info("Error refreshing token: ", reason);
           });
       };
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -310,10 +312,10 @@ export default function App() {
       {loading ? (
         <Box
           sx={{
-            display: 'flex',
-            height: '100%',
-            justifyContent: 'center',
-            alignItems: 'center',
+            display: "flex",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
             background: theme.palette.neutral[0],
           }}
         >
@@ -346,11 +348,12 @@ export default function App() {
 export function ErrorBoundary({ error }: { error: Error }) {
   const navigate = useNavigate();
   const { t } = useTranslation();
-  logger(error, 'app/root', undefined);
+  const { metas } = useService();
+  logger(error, "app/root", undefined);
 
   return (
     <Document title="Error!">
-      <Layout>{renderError(navigate, t)}</Layout>
+      <Layout>{renderError(navigate, t, metas.origin)}</Layout>
     </Document>
   );
 }
@@ -359,7 +362,9 @@ export function CatchBoundary() {
   const caught = useCatch();
   const navigate = useNavigate();
   const { t } = useTranslation();
-  logger(caught, 'app/root/CatchBoundary');
+  const { metas } = useService();
+
+  logger(caught, "app/root/CatchBoundary");
 
   const error = camelCase(get(errorsMap, caught.status, errorsMap[422]));
   const message = t(`common.boundaries.errorState.${error}.title`);
@@ -367,7 +372,9 @@ export function CatchBoundary() {
 
   return (
     <Document title={`${caught.status} ${caught.statusText}`}>
-      <Layout>{renderError(navigate, t, message, description)}</Layout>
+      <Layout>
+        {renderError(navigate, t, metas.origin, message, description)}
+      </Layout>
     </Document>
   );
 }
