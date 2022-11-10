080e6c631a4c772e0f49bdb9415683370b97b1f1
reslene <reslene@numary.com>
2022-11-10 17:40:56 +0100

    fix(): current user
    Signed-off-by: reslene <reslene@numary.com>

--- app/src/components/Navbar/Navbar.tsx
+++ app/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from "react";
 
-import { ArrowDropDown, Person } from '@mui/icons-material';
+import { ArrowDropDown, Person } from "@mui/icons-material";
 import {
   Avatar,
   Box,
@@ -9,32 +9,60 @@ import {
   MenuItem,
   Typography,
   useTheme,
-} from '@mui/material';
-import { useTranslation } from 'react-i18next';
-import { useLocation, useNavigate } from 'react-router-dom';
+} from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { useLocation, useNavigate } from "react-router-dom";
 
-import Search from './../Search';
+import Search from "./../Search";
 
-import { Navbar as FormanceNavbar } from '@numaryhq/storybook';
+import { Navbar as FormanceNavbar } from "@numaryhq/storybook";
 
-import LinkWrapper from '../Wrappers/LinkWrapper';
+import LinkWrapper from "../Wrappers/LinkWrapper";
 
 import {
   getRoute,
   OVERVIEW_ROUTE,
   routerConfig,
-} from '~/src/components/Navbar/routes';
-import { useService } from '~/src/hooks/useService';
+} from "~/src/components/Navbar/routes";
+import { useService } from "~/src/hooks/useService";
+import { CurrentUser } from "~/src/utils/api";
 
 const Navbar: FunctionComponent = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { t } = useTranslation();
   const theme = useTheme();
-  const { currentUser, metas } = useService();
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
     null
   );
+  const { api, setCurrentUser, currentUser, metas } = useService();
+
+  const getCurrentUser = async () => {
+    try {
+      const user = await api.getResource<CurrentUser>(
+        `${metas.openIdConfig.userinfo_endpoint.split("api")[1]}`
+      );
+      if (user) {
+        const pseudo =
+          user && user.email ? user.email.split("@")[0] : undefined;
+
+        setCurrentUser({
+          ...user,
+          avatarLetter: pseudo ? pseudo.split("")[0].toUpperCase() : undefined,
+          pseudo,
+        });
+      }
+    } catch (e) {
+      console.info("Current user could not be retrieved");
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      await getCurrentUser();
+    })();
+  }, []);
+
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
@@ -54,7 +82,7 @@ const Navbar: FunctionComponent = () => {
     label: t(route.label),
   }));
 
-  const settings = [t('topbar.logout')];
+  const settings = [t("topbar.logout")];
 
   return (
     <FormanceNavbar
@@ -63,17 +91,17 @@ const Navbar: FunctionComponent = () => {
       routes={formattedRouterConfig}
       location={location}
       linkWrapper={
-        <LinkWrapper to={''} prefetch={'none'} color={''}>
+        <LinkWrapper to={""} prefetch={"none"} color={""}>
           <Box />
         </LinkWrapper>
       }
     >
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          width: '120px',
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "120px",
         }}
       >
         <Search />
@@ -99,17 +127,17 @@ const Navbar: FunctionComponent = () => {
               <ArrowDropDown sx={{ color: theme.palette.grey[500] }} />
             </IconButton>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
