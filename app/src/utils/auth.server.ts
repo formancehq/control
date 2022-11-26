08191fa8fa86615675cd3d64989a4d647b793a1c
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2022-11-26 23:00:39 +0100

    feat: expose cookie over plain http if not in production

--- app/src/utils/auth.server.ts
+++ app/src/utils/auth.server.ts
@@ -29,7 +29,7 @@ export const sessionStorage = createCookieSessionStorage({
     name: COOKIE_NAME, // use any name you want here
     sameSite: 'lax', // this helps with CSRF
     path: '/', // remember to add this so the cookie will work in all routes
-    httpOnly: true, // for security reasons, make this cookie http only
+    httpOnly: process.env.NODE_ENV === 'production', // for security reasons, make this cookie http only
     secrets: [process.env.CLIENT_SECRET || 'secret'], // replace this with an actual secret
     secure: process.env.NODE_ENV === 'production',
   },
