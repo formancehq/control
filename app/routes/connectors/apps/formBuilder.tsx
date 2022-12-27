54b998325b414fc72a172365f6d17e27d43c8a44
reslene <reslene@numary.com>
2022-12-27 15:35:04 +0100

    chore(Apps): use config from API instead of hardcoded one. Throw form error and prevent creating connector when trying to add a connector already installed. Refactor some code and type. Fix redirection issue on connector creation. Add new connector logos. Use payment api config instead of hardcoded one (for apps page and for payment provider filter list)
    Signed-off-by: reslene <reslene@numary.com>

--- app/routes/connectors/apps/formBuilder.tsx
+++ app/routes/connectors/apps/formBuilder.tsx
@@ -1,70 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { capitalize, lowerCase } from 'lodash';
-import { Controller } from 'react-hook-form';
+import { capitalize, lowerCase } from "lodash";
+import { Controller } from "react-hook-form";
 
-import { TextField } from '@numaryhq/storybook';
+import { TextField } from "@numaryhq/storybook";
 
-import i18n from '~/src/translations';
+import i18n from "~/src/translations";
 import {
   ConnectorConfigFormProps,
   InputType,
-} from '~/src/types/connectorsConfig';
-import { ObjectOf } from '~/src/types/generic';
+} from "~/src/types/connectorsConfig";
+import { ObjectOf } from "~/src/types/generic";
 
 // TODO this config is hardcoded for now but the idea is to fetch it from the API
 // so the backend can change the config without having to update the frontend
 
 export enum FormTypes {
-  STRING = 'string',
-  DURATION = 'duration ns',
-  INTEGER = 'integer',
+  STRING = "string",
+  DURATION = "duration ns",
+  INTEGER = "integer",
 }
-export const connectorsConfig = {
-  dummypay: {
-    directory: {
-      datatype: FormTypes.STRING,
-      required: true,
-    },
-    fileGenerationPeriod: {
-      datatype: FormTypes.DURATION,
-    },
-    filePollingPeriod: {
-      datatype: FormTypes.DURATION,
-    },
-  },
-  modulr: {
-    apiKey: {
-      datatype: FormTypes.STRING,
-      required: true,
-    },
-    apiSecret: {
-      datatype: FormTypes.STRING,
-      required: true,
-    },
-    endpoint: {
-      datatype: FormTypes.STRING,
-    },
-  },
-  stripe: {
-    apiKey: {
-      datatype: FormTypes.STRING,
-      required: true,
-    },
-    pollingPeriod: {
-      datatype: FormTypes.DURATION,
-    },
-    pageSize: {
-      datatype: 'integer',
-    },
-  },
-  wise: {
-    apiKey: {
-      datatype: FormTypes.STRING,
-      required: true,
-    },
-  },
-};
 
 const inputsFactory = ({
   inputConfig,
@@ -88,7 +43,7 @@ const inputsFactory = ({
     error: !!errors?.[parentName]?.[name],
     errorMessage:
       errors?.[parentName]?.[name] &&
-      i18n.t('common.formErrorsMessage.requiredInputs', {
+      i18n.t("common.formErrorsMessage.requiredInputs", {
         inputName: lowerCase(name),
       }),
   };
@@ -100,7 +55,7 @@ const inputsFactory = ({
     rules: { required: inputConfig.required },
   };
 
-  switch (inputConfig.datatype) {
+  switch (inputConfig.dataType) {
     case FormTypes.STRING:
       return (
         <Controller
@@ -127,8 +82,8 @@ const inputsFactory = ({
       );
     default:
       throw new Error(
-        i18n.t('pages.apps.form.errors.inputTypeDoesntExist', {
-          fieldType: inputConfig.datatype,
+        i18n.t("pages.apps.form.errors.inputTypeDoesntExist", {
+          fieldType: inputConfig.dataType,
         })
       );
   }
--- app/src/types/connectorsConfig.ts
+++ app/src/types/connectorsConfig.ts
@@ -1,22 +1,16 @@
-import { ObjectOf } from './generic';
+import { ObjectOf } from "./generic";
 
 export type InputType = {
-  datatype: string;
+  dataType: string;
   required?: boolean;
 };
 
 export type ConnectorConfigFormProps = {
-  stripe: Record<string, InputType>;
-  modulr: Record<string, InputType>;
-  dummypay: Record<string, InputType>;
-  wise: Record<string, InputType>;
+  [name: string]: Record<string, InputType>;
 };
 
 export type ConnectorFormValues = {
-  stripe: ObjectOf<string>;
-  modulr: ObjectOf<string>;
-  dummypay: ObjectOf<string>;
-  wise: ObjectOf<string>;
+  [name: string]: ObjectOf<string>;
 };
 
 export type Connector = {
@@ -28,7 +22,7 @@ export type ConnectorTask = {
   provider: string;
   descriptor: { main: boolean };
   createdAt: Date;
-  status: 'active' | 'error';
+  status: "active" | "error";
   error: string;
   state?: string;
   id: string;
