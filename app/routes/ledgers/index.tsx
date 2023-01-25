4edb8f54930ecafba60ff190e647ac1972eb555f
reslene <reslene@numary.com>
2023-01-25 16:44:19 +0100

    feat(Error): add missing errors boundaries
    Signed-off-by: reslene <reslene@numary.com>

--- app/routes/ledgers/index.tsx
+++ app/routes/ledgers/index.tsx
@@ -1,34 +1,46 @@
-import * as React from 'react';
+import * as React from "react";
 
-import type { MetaFunction } from '@remix-run/node';
-import { Session } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
-import { LoaderFunction } from '@remix-run/server-runtime';
-import { useTranslation } from 'react-i18next';
+import type { MetaFunction } from "@remix-run/node";
+import { Session } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { LoaderFunction } from "@remix-run/server-runtime";
+import { useTranslation } from "react-i18next";
 
-import { Page, Row } from '@numaryhq/storybook';
+import { Page, Row } from "@numaryhq/storybook";
 
 import {
   LEDGER_ROUTE,
   ledgers as ledgersConfig,
-} from '~/src/components/Layout/routes';
-import ShowListAction from '~/src/components/Wrappers/Lists/Actions/ShowListAction';
-import Table from '~/src/components/Wrappers/Table';
-import { API_LEDGER } from '~/src/utils/api';
-import { createApiClient } from '~/src/utils/api.server';
-import { handleResponse, withSession } from '~/src/utils/auth.server';
+} from "~/src/components/Layout/routes";
+import ShowListAction from "~/src/components/Wrappers/Lists/Actions/ShowListAction";
+import Table from "~/src/components/Wrappers/Table";
+import { API_LEDGER } from "~/src/utils/api";
+import { createApiClient } from "~/src/utils/api.server";
+import { handleResponse, withSession } from "~/src/utils/auth.server";
+import ComponentErrorBoundary from "~/src/components/Wrappers/ComponentErrorBoundary";
 
 export const meta: MetaFunction = () => ({
-  title: 'Ledgers',
-  description: 'List',
+  title: "Ledgers",
+  description: "List",
 });
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <ComponentErrorBoundary
+      id="ledgers"
+      title="pages.ledgers.title"
+      error={error}
+      showAction={false}
+    />
+  );
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
   async function handleData(session: Session) {
     const api = await createApiClient(session);
     const ledgers = await api.getResource<string[]>(
       `${API_LEDGER}/_info`,
-      'data.config.storage.ledgers'
+      "data.config.storage.ledgers"
     );
     if (ledgers) {
       return ledgers.map((ledger: string) => ({ name: ledger }));
@@ -53,14 +65,14 @@ export default function Index() {
         withPagination={false}
         columns={[
           {
-            key: 'name',
-            label: t('pages.ledgers.table.columnLabel.name'),
+            key: "name",
+            label: t("pages.ledgers.table.columnLabel.name"),
           },
         ]}
         renderItem={(ledger: { name: string }, index: number) => (
           <Row
             key={index}
-            keys={['name']}
+            keys={["name"]}
             item={ledger}
             renderActions={() => (
               <ShowListAction id={ledger.name} route={LEDGER_ROUTE} />
