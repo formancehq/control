508962059da6ca5dd57da698b98e3233dd596729
reslene <reslene@numary.com>
2023-01-26 19:35:47 +0100

    fix(Logs): fix typo
    Signed-off-by: reslene <reslene@numary.com>

--- app/routes/ledgers/$ledgerId/logs/index.tsx
+++ app/routes/ledgers/$ledgerId/logs/index.tsx
@@ -42,7 +42,7 @@ export const loader: LoaderFunction = async ({ request, params }) => {
     invariant(params.ledgerId, 'Expected params.ledgerId');
     const api = await createApiClient(session);
     const query = sanitizeQuery(request, QueryContexts.PARAMS);
-    const url = `${API_LEDGER}/${params.ledgerId}/log?${query}`;
+    const url = `${API_LEDGER}/${params.ledgerId}/logs?${query}`;
     const logs = await api.getResource<
       Cursor<LedgerLog<Transaction | ObjectOf<any>>>
     >(url, 'cursor');
