55acb0843e6535de54ebb2d948b62535ad9e18cb
reslene <reslene@numary.com>
2023-03-29 17:00:44 +0200

    feat(InstanceDetailedPage): handle toggle for lower node + adjust a bit the UI.
    Signed-off-by: reslene <reslene@numary.com>

--- app/src/components/Wrappers/Workflows/CustomNode/NodeTitle/NodeTitle.tsx
+++ app/src/components/Wrappers/Workflows/CustomNode/NodeTitle/NodeTitle.tsx
@@ -1,20 +1,45 @@
 import React, { FunctionComponent } from 'react';
 
-import { Typography } from '@mui/material';
+import { ChevronRight, ExpandMore } from '@mui/icons-material';
+import { Box, Typography } from '@mui/material';
+
+import { LoadingButton } from '@numaryhq/storybook';
 
 import { NodeTitleProps } from '~/src/components/Wrappers/Workflows/CustomNode/NodeTitle/types';
+import { useToggle } from '~/src/hooks/useToggle';
 
-const NodeTitle: FunctionComponent<NodeTitleProps> = ({ label, color }) => (
-  <Typography
-    sx={{
-      p: 0.5,
-      background: ({ palette }) => (color ? color : palette.neutral[50]),
-      mb: 1,
-      borderRadius: '4px',
-    }}
-  >
-    {label}
-  </Typography>
-);
+const NodeTitle: FunctionComponent<NodeTitleProps> = ({
+  label,
+  color,
+  onToggle,
+}) => {
+  const [expanded, expand] = useToggle(false);
+  const icon = expanded ? <ExpandMore /> : <ChevronRight />;
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        p: 0.5,
+        background: ({ palette }) => (color ? color : palette.neutral[50]),
+        borderRadius: '6px',
+      }}
+    >
+      <Typography>{label}</Typography>
+      {onToggle && (
+        <LoadingButton
+          startIcon={icon}
+          variant="transparent"
+          onClick={() => {
+            expand();
+            onToggle();
+          }}
+        />
+      )}
+    </Box>
+  );
+};
 
 export default NodeTitle;
